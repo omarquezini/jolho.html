31a1d1a6025fb229e0a1702611196d3b81a51282
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2025-11-15 17:41:06 +0000

    terminando.

---
+++ script.js
@@ -0,0 +1,7 @@
+function acender() {
+    document.body.style.backgroundColor = "yellow"; // muda o fundo
+}
+
+function apagar() {
+    document.body.style.backgroundColor = "black"; // muda o fundo
+}
